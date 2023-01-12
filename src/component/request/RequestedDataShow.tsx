d1c2cd08b84e2fb1408e15b32c9a9591b90f9c4a
mubashar <hmubashar639@gmail.com>
2023-01-13 01:21:20 +0500

    change made

--- src/component/request/RequestedDataShow.tsx
+++ src/component/request/RequestedDataShow.tsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useGetRequest from "../../hooks/useGetPostRequest";
 import "./requestedDataShow.css";
 
 export default function RequestedDataShow() {
-  const { loading, postRequestedData } = useGetRequest(1);
+  const [postId, setValue] = useState(1);
+  const [color, setColor] = useState("red");
+  let wordArr = ["one", "Two", "three", "three", "One", "tWo", "onE", "five"];
+  const [currentColor, setCurrenctColor] = useState("#282c34");
+
+  const [object, setObject] = useState<{ [key: string]: number }>({});
+
+  useEffect(() => {
+    let resultObj: { [key: string]: number } = {};
+
+    for (let latter of wordArr) {
+      if (resultObj.hasOwnProperty(latter.toLocaleLowerCase())) {
+        resultObj[latter.toLocaleLowerCase()] =
+          resultObj[latter.toLocaleLowerCase()] + 1;
+      } else {
+        resultObj[latter.toLocaleLowerCase()] = 1;
+      }
+      setObject(resultObj);
+    }
+  }, []);
+
+  const { loading, postRequestedData } = useGetRequest(postId);
   return (
-    <div className="requestData">
+    <div className="requestData" style={{ background: currentColor }}>
+      <input
+        name="postID"
+        value={postId}
+        type="number"
+        onChange={(e: any) => setValue(e.target.value)}
+      />
+
+      <input
+        name="color"
+        value={color}
+        type="text"
+        onChange={(e: any) => setColor(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setCurrenctColor(color);
+        }}
+      >
+        {" "}
+        set color
+      </button>
       {loading ? (
         "Loading"
       ) : (
@@ -16,6 +58,23 @@ export default function RequestedDataShow() {
           <p>User Id:{postRequestedData?.userId}</p>
         </div>
       )}
+
+      <>
+        <table className="table-color">
+          <tr>
+            <th>Words</th>
+            <th>Count</th>
+          </tr>
+          {Object.keys(object).map((keyItem) => {
+            return (
+              <tr key={keyItem}>
+                <td>{keyItem}</td>
+                <td>{object[keyItem]}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </>
     </div>
   );
 }
